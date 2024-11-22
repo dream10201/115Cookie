79ecffe90bd210f18d0e2187db88c55269d6eb94
Nas Code <xiuxiu10201@gmail.com>
2024-11-22 16:20:57 +0800

    添加Cookie有效性验证

--- worker.js
+++ worker.js
@@ -1,62 +1,60 @@
-const CID=""
-const SEID=""
-const UID=""
-const domain = [{"url":"https://115.com","domain":".115.com"},{"url":"https://anxia.com","domain":".anxia.com"}]
+const CID = '';
+const SEID = '';
+const UID = '';
+const domain = [
+	{
+		'url': 'https://115.com',
+		'domain': '.115.com'
+	},
+	{
+		'url': 'https://anxia.com',
+		'domain': '.anxia.com'
+	}
+];
 const newC = {
-    url: 'https://115.com',
-    name: '',
-    value: '',
-    domain: '.115.com',
-    expirationDate: 253402300799,
-    path: '/'
+	url: 'https://115.com',
+	name: '',
+	value: '',
+	domain: '.115.com',
+	expirationDate: 253402300799,
+	path: '/'
 };
 function setCookie(newC) {
-    return new Promise((resolve, reject) => {
-        chrome.cookies.set(newC, cookie => {
-            if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError);
-            }
-            else if (!cookie) {
-                reject(new Error('This cookie is deleted (expired)'));
-            }
-            else {
-                resolve(cookie);
-            }
-        });
-    });
+	return new Promise((resolve, reject) => {
+		chrome.cookies.set(newC, cookie => {
+			if (chrome.runtime.lastError) {
+				reject(chrome.runtime.lastError);
+			} else if (!cookie) {
+				reject(new Error('This cookie is deleted (expired)'));
+			} else {
+				resolve(cookie);
+			}
+		});
+	});
 }
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "greet") {
-        console.log("收到消息:", message);
-        chrome.cookies.get({ url: "https://115.com", name: "SEID" }, (cookie) => {
-            if (CID.length > 0 && SEID.length > 0 && UID.length > 0){
-                for(let i=0;i<domain.length;i++){
-                    newC["url"] = domain[i]["url"];
-                    newC["domain"] = domain[i]["domain"];
-                    
-                    newC["name"] = "CID";
-                    newC["value"] = CID;
-                    setCookie(newC);
-                    newC["name"] = "SEID";
-                    newC["value"] = SEID;
-                    setCookie(newC);
-                    newC["name"] = "UID";
-                    newC["value"] = UID;
-                    setCookie(newC);
-                }
-                // if (!cookie && !reloadFlag) {
-                //     reloadFlag = !reloadFlag;
-                //     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                //         if (tabs[0]) {
-                //             chrome.tabs.reload(tabs[0].id);
-                //         }
-                //     });
-                // }
-            }
-        });
-        const responseMessage = { reply: "你好，来自后台脚本的问候!" };
-        sendResponse(responseMessage);
-    }
-    return true;
+	if (message.action === 'greet') {
+		fetch('https://115.com/?ct=offline&ac=space').then(response => {
+			if (response.redirected) {
+				sendResponse('offline');
+			} else {
+				if (CID.length > 0 && SEID.length > 0 && UID.length > 0) {
+					for (let i = 0; i < domain.length; i++) {
+						newC['url'] = domain[i]['url'];
+						newC['domain'] = domain[i]['domain'];
+						newC['name'] = 'CID';
+						newC['value'] = CID;
+						setCookie(newC);
+						newC['name'] = 'SEID';
+						newC['value'] = SEID;
+						setCookie(newC);
+						newC['name'] = 'UID';
+						newC['value'] = UID;
+						setCookie(newC);
+					}
+				}
+			}
+		});
+	}
+	return true;
 });
-
