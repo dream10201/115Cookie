c5bf0eae991902a008ffa6ba82b7bd44aefe4557
dream10201 <xiuxiu10201@gmail.com>
2025-07-28 09:45:17 +0800

    update

--- worker.js
+++ worker.js
@@ -11,8 +11,6 @@ const CONFIG = {
     },
     EXPIRATION_DATE: 253402300799 
 };
-
-let COOKIE_STATE=true;
 async function setCookie(cookieDetails) {
     return new Promise((resolve, reject) => {
         chrome.cookies.set(cookieDetails, (cookie) => {
@@ -58,22 +56,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'greet') {
         (async () => {
             try {
-                if(COOKIE_STATE){
-                    await applyCookies();
-                }
-                await delay(5000);
-                const response = await fetch('https://115.com/?cid=0&offset=0&mode=wangpan');
-                
-                if (response.redirected) {
-                    COOKIE_STATE=false;
-                    console.log('Login check failed: Redirected.');
-                    sendResponse({ status: 'login_failed' });
-                } else {
-                    console.log('Login check successful.');
-                    sendResponse({ status: 'login_success' });
-                }
+                await applyCookies();
+                sendResponse({ status: 'applyCookies' });
+                // await delay(5000);
+                // const response = await fetch('https://115.com/?cid=0&offset=0&mode=wangpan');
+                // if (response.redirected) {
+                //     sendResponse({ status: 'login_failed' });
+                // } else {
+                //     sendResponse({ status: 'login_success' });
+                // }
             } catch (error) {
-                console.error('An error occurred during the process:', error);
                 sendResponse({ status: 'error', message: error.message });
             }
         })();
